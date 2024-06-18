693147d0c2afadc17cf58c00096238128fbf6104
Rajput <nitin98811234@gmail.com>
2024-06-18 11:35:13 +0530

    Adding a header & State management

--- 02-React-Refresher/starting-project/src/App.jsx
+++ 02-React-Refresher/starting-project/src/App.jsx
@@ -1,10 +1,22 @@
+import { useState } from "react";
 import PostLists from "./components/PostLists";
+import MainHeader from "./components/MainHeader";
 
 function App() {
+  const [modalIsVisible, setModalIsVisible] = useState(false);
+  const showModalHandler = () => setModalIsVisible(true);
+
+  const hideModalHandler = () => setModalIsVisible(false);
   return (
-    <main>
-      <PostLists />
-    </main>
+    <>
+      <MainHeader onCreatePost={showModalHandler} />
+      <main>
+        <PostLists
+          isPosting={modalIsVisible}
+          onStopPosting={hideModalHandler}
+        />
+      </main>
+    </>
   );
 }
 
--- 02-React-Refresher/starting-project/src/components/PostLists.jsx
+++ 02-React-Refresher/starting-project/src/components/PostLists.jsx
@@ -1,13 +1,33 @@
 import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostLists.module.css";
+import { useState } from "react";
+import Modal from "./Modal";
 
-function PostLists() {
+function PostLists({ isPosting, onStopPosting }) {
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  const bodyChangeHandler = (event) => {
+    setEnteredBody(event.target.value);
+  };
+
+  const authorChangeHandler = (event) => {
+    setEnteredAuthor(event.target.value);
+  };
   return (
     <>
-      <NewPost />
+      {isPosting && (
+        <Modal onClose={onStopPosting}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
       <ul className={classes.posts}>
-        <Post author="Nitin Rajput" body="Text 1" />
+        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Kapil" body="Text 2" />
       </ul>
     </>
